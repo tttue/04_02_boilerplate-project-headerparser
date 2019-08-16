cd4a5baca8a30f3a2f6f56fb8fcb46c96b7344bf
Trần Trọng Tuệ <tttue@yahoo.com>
2019-08-16 10:36:40 +0900

    Add lib

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 // server.js
 // where your node app starts
+/*
+	npm install express cors
+*/
 
 // init project
 var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -19,7 +22,7 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
